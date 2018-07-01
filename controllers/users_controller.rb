53625d75f1432a5b6760a0fa4680aaf644347082
pimk333 <pikugift@gmail.com>
2018-07-01 13:39:41 +0000

    12

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
-
-  # GET /users/:id
+  before_action :logged_in_user, only: [:index,:edit, :update,:destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+     #有効なユーザーだけを表示するコードのテンプレート
+  def index
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+   
+   #有効なユーザーだけを表示するコードのテンプレート
   def show
     @user = User.find(params[:id])
-    # => app/views/users/show.html.erb
-    # debugger
+    redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -15,24 +28,65 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    if @user.save # => Validation
-      # Sucess
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      # GET "/users/#{@user.id}" => show
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
-      # Failure
       render 'new'      
     end
   end
   
+  def edit 
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+    private
+    
   def user_params
     params.require(:user).permit(
       :name, :email, :password, 
       :password_confirmation)
   end
+  
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+  
+  def logged_in_user
+      unless logged_in?
+       store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+  
+# 正しいユーザーかどうか確認
+#別のユーザーのプロフィールを編集しようとしたらリダイレクトさせたいので、
+      
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user?(@user)
+  end
+
+  private
+      # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
+
 
 
 
--- models/user.rb
+++ models/user.rb
@@ -1,14 +1,29 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },allow_nil: true
 
+
+  # アカウントを有効にする
+  def activate
+    #update_attribute(:activated,    true)
+    #update_attribute(:activated_at, Time.zone.now)
+     update_columns(activated: FILL_IN, activated_at: FILL_IN)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -28,20 +43,65 @@ class User < ApplicationRecord
     User.digest(remember_token))
   end
   
-   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
-    # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
+
 
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+        # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
+  
+    # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
+  
+  private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+    
+
+
+
+    
+    
   
 end
